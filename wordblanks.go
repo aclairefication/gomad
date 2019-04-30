79d6a751befb906a35ebfbb220338d231e333944
Claire Moss <aclairefication@gmail.com>
2019-04-29 20:09:51 -0400

    Extracted build and send request methods

--- wordblanks.go
+++ wordblanks.go
@@ -14,72 +14,81 @@ type ReturnedWords struct {
 	Adjectives   []string `json:"adjective"`
 }
 
-func callwordblanks() {
-	//Word Blanks Mad Libs generator
+func buildwordBlanksURL() string {
+
+	token := "2UdLWTsozqnPNltOB4n2h6X8nf7lAH3m"
 
 	//Hardcoding is so boring
+	// URL for GET from https://www.wordblanks.com/mad-libs/computing/moss-memory/
+	// wordBlanksURL := "https://www.wordblanks.com/scripts/wb_ajax.php?token=diZo19~pw9wzzTmjssdNBAcJ2SQECyG8Ip.LwefrOnM-&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
 	// partsofspeech := "&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
-	// wordblanksurl := "https://www.wordblanks.com/scripts/wb_ajax.php?token=diZo19~pw9wzzTmjssdNBAcJ2SQECyG8Ip.LwefrOnM-&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
+	// wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + partsofspeech
 
 	wordblanksapibase := "https://www.wordblanks.com/scripts/wb_ajax.php?"
-	token := "2UdLWTsozqnPNltOB4n2hzGkGKd6Ome4"
-	//Expired	token := "diZo19~pw9wzzTmjssdNBAcJ2SQECyG8Ip.LwefrOnM-"
-	//Getting words
 	method := "getRandomWordSet"
 
-	// Want an slice of parts of speech and then range across to build this URL
-	parts_of_speech := []string{"noun", "verb_present", "verb", "adjective"}
+	// Want a slice of parts of speech and then range across to build this URL
+	partsOfSpeech := []string{"noun", "verb_present", "verb", "adjective"}
 	var speechparams string
-	for _, part := range parts_of_speech {
+	for _, part := range partsOfSpeech {
 		speechparams = speechparams + "&posArray%5B%5D=" + part
 	}
-	// fmt.Println("Parts of speech are", speechparams)
 
 	fmt.Println("Calling Word Blanks")
 
-	// wordblanksurl := wordblanksapibase + "token=" + token + "&" + "method=" + method + partsofspeech
-	wordblanksurl := wordblanksapibase + "token=" + token + "&" + "method=" + method + speechparams
-	fmt.Println("Request to URL ", wordblanksurl)
+	wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + speechparams
+	fmt.Println("Request to URL ", wordBlanksURL)
 
-	//Modified code from https://medium.com/@IndianGuru/consuming-json-apis-with-go-d711efc1dcf9
+	return wordBlanksURL
+}
 
-	var record ReturnedWords
+//Modified code from https://medium.com/@IndianGuru/consuming-json-apis-with-go-d711efc1dcf9
 
+func buildRequest(requestMethod string, wordBlanksURL string) *http.Request {
 	// Build the request
-	req, err := http.NewRequest("GET", wordblanksurl, nil)
+	request, err := http.NewRequest("GET", wordBlanksURL, nil)
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
-		return
 	}
+	return request
+}
 
-	// For control over HTTP client headers,
-	// redirect policy, and other settings,
-	// create a Client
+func sendRequest(request *http.Request, client http.Client) *http.Response {
+	// Send the request via a client
+	// Do sends an HTTP request and returns an HTTP response
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+	return response
+}
+
+func callWordBlanks() {
+
+	wordBlanksURL := buildwordBlanksURL()
+
+	var madLibWords ReturnedWords
+
+	request := buildRequest("GET", wordBlanksURL)
+
+	// For control over HTTP client headers, redirect policy, and other settings, create a Client
 	// A Client is an HTTP client
 	client := &http.Client{}
 
-	// Send the request via a client
-	// Do sends an HTTP request and
-	// returns an HTTP response
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Do: ", err)
-		return
-	}
+	response := sendRequest(request, *client)
 
-	// Callers should close resp.Body
-	// when done reading from it
+	// Callers should close response.Body when done reading from it
 	// Defer the closing of the body
-	defer resp.Body.Close()
+	defer response.Body.Close()
 
 	// Use json.Decode for reading streams of JSON data
-	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+	if err := json.NewDecoder(response.Body).Decode(&madLibWords); err != nil {
 		log.Println(err)
 	}
 
-	fmt.Println("Nouns list = ", record.Nouns)
-	fmt.Println("Present tense verbs = ", record.PresentVerbs)
-	fmt.Println("Verbs = ", record.Verbs)
-	fmt.Println("Adjectives = ", record.Adjectives)
+	fmt.Println("Nouns list = ", madLibWords.Nouns)
+	fmt.Println("Present tense verbs = ", madLibWords.PresentVerbs)
+	fmt.Println("Verbs = ", madLibWords.Verbs)
+	fmt.Println("Adjectives = ", madLibWords.Adjectives)
 
 }
