19208e3fe15b80f4f524d456b792231a5843c00c
Claire Moss <aclairefication@gmail.com>
2019-05-08 23:12:09 -0400

    what is your deal gofmt

--- gomad.go
+++ gomad.go
@@ -1,64 +1,64 @@
 package main
 
 import (
-        "fmt"
-        "strings"
+	"fmt"
+	"strings"
 
-        "github.com/castillobgr/sententia"
+	"github.com/castillobgr/sententia"
 )
 
 func generateSentence() string {
-        // fmt.Println("Calling generateSentence")
-        //Using sententia to generate Mad Libs with adjectives & nouns
-        sentence, err := sententia.Make("Aw yis, {{ adjective }} {{ nouns }}.")
-        if err != nil {
-                panic(err)
-        }
-        return sentence
+	// fmt.Println("Calling generateSentence")
+	//Using sententia to generate Mad Libs with adjectives & nouns
+	sentence, err := sententia.Make("Aw yis, {{ adjective }} {{ nouns }}.")
+	if err != nil {
+		panic(err)
+	}
+	return sentence
 }
 
 func fillSentence(sentenceWithBlanks string) string {
-        fmt.Println("Calling fillSentence with '" + sentenceWithBlanks + "'")
-        sentence, err := sententia.Make(sentenceWithBlanks)
-        if err != nil {
-                panic(err)
-        }
-        fmt.Println("fillSentence created '" + sentence + "'")
-        return sentence
+	fmt.Println("Calling fillSentence with '" + sentenceWithBlanks + "'")
+	sentence, err := sententia.Make(sentenceWithBlanks)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("fillSentence created '" + sentence + "'")
+	return sentence
 }
 
 func main() {
-        //Function with hard-coded string pattern and no return
-        callWordBlanks()
+	//Function with hard-coded string pattern and no return
+	callWordBlanks()
 
-        //Function with hard-coded string pattern and result return
-        madlib := generateSentence()
-        fmt.Println("madlib sentence is " + madlib)
+	//Function with hard-coded string pattern and result return
+	madlib := generateSentence()
+	fmt.Println("madlib sentence is " + madlib)
 
-        //Function with dynamic input and result return
-        sentenceToFill := "Aw yis, {{ adjective }} {{ nouns }}."
-        completedSentence := fillSentence(sentenceToFill)
-        fmt.Println("another madlib sentence: " + completedSentence)
+	//Function with dynamic input and result return
+	sentenceToFill := "Aw yis, {{ adjective }} {{ nouns }}."
+	completedSentence := fillSentence(sentenceToFill)
+	fmt.Println("another madlib sentence: " + completedSentence)
 
-        //Function with dynamic input and result return
-        sentenceToFill2 := "Oh look, {{ an adjective }} {{ noun }}!"
-        completedSentence2 := fillSentence(sentenceToFill2)
-        fmt.Println("another madlib sentence: " + completedSentence2)
+	//Function with dynamic input and result return
+	sentenceToFill2 := "Oh look, {{ an adjective }} {{ noun }}!"
+	completedSentence2 := fillSentence(sentenceToFill2)
+	fmt.Println("another madlib sentence: " + completedSentence2)
 
-        //Function with dynamic input and result return that uses Actions from Go's text/template package
-        //Define new action
-        customActions := map[string]interface{}{
-                "capitalize": strings.Title,
-        }
-        sententia.AddActions(customActions)
-        //Use new action
-        completedSentence3, err := sententia.Make(
-                "She wrote a book called '{{ capitalize (an adjective) }} {{ capitalize noun }}'",
-        )
-        if err != nil {
-                panic(err)
-        } else {
-                fmt.Println("actions madlib: " + completedSentence3)
-        }
+	//Function with dynamic input and result return that uses Actions from Go's text/template package
+	//Define new action
+	customActions := map[string]interface{}{
+		"capitalize": strings.Title,
+	}
+	sententia.AddActions(customActions)
+	//Use new action
+	completedSentence3, err := sententia.Make(
+		"She wrote a book called '{{ capitalize (an adjective) }} {{ capitalize noun }}'",
+	)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("actions madlib: " + completedSentence3)
+	}
 
 }
--- wordblanks.go
+++ wordblanks.go
@@ -1,94 +1,94 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "log"
-        "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type returnedWords struct {
-        Nouns        []string `json:"noun"`
-        PresentVerbs []string `json:"verb_present"`
-        Verbs        []string `json:"verb"`
-        Adjectives   []string `json:"adjective"`
+	Nouns        []string `json:"noun"`
+	PresentVerbs []string `json:"verb_present"`
+	Verbs        []string `json:"verb"`
+	Adjectives   []string `json:"adjective"`
 }
 
 func buildwordBlanksURL() string {
 
-        token := "2UdLWTsozqnPNltOB4n2h6X8nf7lAH3m"
+	token := "2UdLWTsozqnPNltOB4n2h6X8nf7lAH3m"
 
-        //Hardcoding is so boring
-        // URL for GET from https://www.wordblanks.com/mad-libs/computing/moss-memory/
-        // wordBlanksURL := "https://www.wordblanks.com/scripts/wb_ajax.php?token=diZo19~pw9wzzTmjssdNBAcJ2SQECyG8Ip.LwefrOnM-&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
-        // partsofspeech := "&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
-        // wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + partsofspeech
+	//Hardcoding is so boring
+	// URL for GET from https://www.wordblanks.com/mad-libs/computing/moss-memory/
+	// wordBlanksURL := "https://www.wordblanks.com/scripts/wb_ajax.php?token=diZo19~pw9wzzTmjssdNBAcJ2SQECyG8Ip.LwefrOnM-&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
+	// partsofspeech := "&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
+	// wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + partsofspeech
 
-        wordblanksapibase := "https://www.wordblanks.com/scripts/wb_ajax.php?"
-        method := "getRandomWordSet"
+	wordblanksapibase := "https://www.wordblanks.com/scripts/wb_ajax.php?"
+	method := "getRandomWordSet"
 
-        // Want a slice of parts of speech and then range across to build this URL
-        partsOfSpeech := []string{"noun", "verb_present", "verb", "adjective"}
-        var speechparams string
-        for _, part := range partsOfSpeech {
-                speechparams = speechparams + "&posArray%5B%5D=" + part
-        }
+	// Want a slice of parts of speech and then range across to build this URL
+	partsOfSpeech := []string{"noun", "verb_present", "verb", "adjective"}
+	var speechparams string
+	for _, part := range partsOfSpeech {
+		speechparams = speechparams + "&posArray%5B%5D=" + part
+	}
 
-        fmt.Println("Calling Word Blanks")
+	fmt.Println("Calling Word Blanks")
 
-        wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + speechparams
-        fmt.Println("Request to URL ", wordBlanksURL)
+	wordBlanksURL := wordblanksapibase + "token=" + token + "&" + "method=" + method + speechparams
+	fmt.Println("Request to URL ", wordBlanksURL)
 
-        return wordBlanksURL
+	return wordBlanksURL
 }
 
 //Modified code from https://medium.com/@IndianGuru/consuming-json-apis-with-go-d711efc1dcf9
 
 func buildRequest(requestMethod string, wordBlanksURL string) *http.Request {
-        // Build the request
-        request, err := http.NewRequest("GET", wordBlanksURL, nil)
-        if err != nil {
-                log.Fatal("NewRequest: ", err)
-        }
-        return request
+	// Build the request
+	request, err := http.NewRequest("GET", wordBlanksURL, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+	}
+	return request
 }
 
 func sendRequest(request *http.Request, client http.Client) *http.Response {
-        // Send the request via a client
-        // Do sends an HTTP request and returns an HTTP response
-        response, err := client.Do(request)
-        if err != nil {
-                log.Fatal("Do: ", err)
-        }
-        return response
+	// Send the request via a client
+	// Do sends an HTTP request and returns an HTTP response
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+	return response
 }
 
 func callWordBlanks() {
 
-        wordBlanksURL := buildwordBlanksURL()
+	wordBlanksURL := buildwordBlanksURL()
 
-        var madLibWords returnedWords
+	var madLibWords returnedWords
 
-        request := buildRequest("GET", wordBlanksURL)
+	request := buildRequest("GET", wordBlanksURL)
 
-        // For control over HTTP client headers, redirect policy, and other settings, create a Client
-        // A Client is an HTTP client
-        client := &http.Client{}
+	// For control over HTTP client headers, redirect policy, and other settings, create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
 
-        response := sendRequest(request, *client)
+	response := sendRequest(request, *client)
 
-        // Callers should close response.Body when done reading from it
-        // Defer the closing of the body
-        defer response.Body.Close()
+	// Callers should close response.Body when done reading from it
+	// Defer the closing of the body
+	defer response.Body.Close()
 
-        // Use json.Decode for reading streams of JSON data
-        if err := json.NewDecoder(response.Body).Decode(&madLibWords); err != nil {
-                log.Println(err)
-        }
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(response.Body).Decode(&madLibWords); err != nil {
+		log.Println(err)
+	}
 
-        fmt.Println("Nouns list = ", madLibWords.Nouns)
-        fmt.Println("Present tense verbs = ", madLibWords.PresentVerbs)
-        fmt.Println("Verbs = ", madLibWords.Verbs)
-        fmt.Println("Adjectives = ", madLibWords.Adjectives)
+	fmt.Println("Nouns list = ", madLibWords.Nouns)
+	fmt.Println("Present tense verbs = ", madLibWords.PresentVerbs)
+	fmt.Println("Verbs = ", madLibWords.Verbs)
+	fmt.Println("Adjectives = ", madLibWords.Adjectives)
 
 }
