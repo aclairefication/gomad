4b3e14bd0430fee5320d9994b56387de10d7bc1f
Claire Moss <aclairefication@gmail.com>
2019-05-08 20:21:06 -0400

    gofmt for the files

--- test/gomad_test.go
+++ test/gomad_test.go
@@ -1,120 +1,121 @@
 // +build unit
+
 package main
 
 import (
-	"strings"
-	"testing"
+        "strings"
+        "testing"
 )
 
 //Generate sentence should return a non-empty string
 func TestGeneratedSentenceIsNotEmpty(t *testing.T) {
-	sentence := generateSentence()
-	if sentence == "" {
-		t.Error("generateSentence was incorrect, got empty string but want not empty string")
-	}
+        sentence := generateSentence()
+        if sentence == "" {
+                t.Error("generateSentence was incorrect, got empty string but want not empty string")
+        }
 }
 
 //Generate sentence should return a string that contains "Aw yis, "
 func TestGeneratedSentenceContainsAwYis(t *testing.T) {
-	sentence := generateSentence()
+        sentence := generateSentence()
 
-	// if sentence != "Aw yis, " {
-	if !strings.Contains(sentence, "Aw yis, ") {
-		t.Error("generateSentence was incorrect. Should have contained 'Aw yis, ' but was '" + sentence + "'")
-	}
+        // if sentence != "Aw yis, " {
+        if !strings.Contains(sentence, "Aw yis, ") {
+                t.Error("generateSentence was incorrect. Should have contained 'Aw yis, ' but was '" + sentence + "'")
+        }
 }
 
 //Generate sentence should return a string starting with "Aw yis, "
 func TestGeneratedSentenceStartsWithAwYis(t *testing.T) {
-	sentence := generateSentence()
+        sentence := generateSentence()
 
-	if !strings.HasPrefix(sentence, "Aw yis, ") {
-		t.Error("generateSentence was incorrect. Should have started with 'Aw yis, ' but was '" + sentence + "'")
-	}
+        if !strings.HasPrefix(sentence, "Aw yis, ") {
+                t.Error("generateSentence was incorrect. Should have started with 'Aw yis, ' but was '" + sentence + "'")
+        }
 }
 
 //Generate sentence should return a sentence ending with a period
 func TestGeneratedSentenceEndsWithAPeriod(t *testing.T) {
-	sentence := generateSentence()
+        sentence := generateSentence()
 
-	if !strings.HasSuffix(sentence, ".") {
-		t.Error("generateSentence was incorrect. Should have ended with a period but was '" + sentence + "'")
-	}
+        if !strings.HasSuffix(sentence, ".") {
+                t.Error("generateSentence was incorrect. Should have ended with a period but was '" + sentence + "'")
+        }
 }
 
 //Generated sentence should contain 4 words
 func TestGeneratedSentenceContainsFourWords(t *testing.T) {
-	sentence := generateSentence()
-	spaceCount := strings.Count(sentence, " ")
+        sentence := generateSentence()
+        spaceCount := strings.Count(sentence, " ")
 
-	if spaceCount != 3 {
-		t.Error("generateSentence was incorrect. Should have contained 3 spaces (i.e. 4 words) but was '" + sentence + "'")
-	}
+        if spaceCount != 3 {
+                t.Error("generateSentence was incorrect. Should have contained 3 spaces (i.e. 4 words) but was '" + sentence + "'")
+        }
 }
 
 //Generate a sentence by passing in a pattern
 func TestFillSentence(t *testing.T) {
-	sentencePattern := "Aw yis, {{ adjective }} {{ nouns }}."
-	sentencePrefix := strings.Split(sentencePattern, "{{")[0]
-	t.Log("Prefix is '" + sentencePrefix + "'")
-	patternsInString := strings.Split(sentencePattern, "}}")
-	numPatterns := len(patternsInString)
-	sentenceSuffix := strings.Split(sentencePattern, "}}")[numPatterns-1]
-	t.Log("Suffix is '" + sentenceSuffix + "'")
+        sentencePattern := "Aw yis, {{ adjective }} {{ nouns }}."
+        sentencePrefix := strings.Split(sentencePattern, "{{")[0]
+        t.Log("Prefix is '" + sentencePrefix + "'")
+        patternsInString := strings.Split(sentencePattern, "}}")
+        numPatterns := len(patternsInString)
+        sentenceSuffix := strings.Split(sentencePattern, "}}")[numPatterns-1]
+        t.Log("Suffix is '" + sentenceSuffix + "'")
 
-	filledSentence := fillSentence(sentencePattern)
+        filledSentence := fillSentence(sentencePattern)
 
-	if !strings.HasPrefix(filledSentence, sentencePrefix) && !strings.HasSuffix(filledSentence, sentenceSuffix) {
-		t.Error("generateSentence was incorrect. Should have started with '" + sentencePrefix + "' and end with '" + sentenceSuffix + "' but was '" + filledSentence + "'")
-	}
+        if !strings.HasPrefix(filledSentence, sentencePrefix) && !strings.HasSuffix(filledSentence, sentenceSuffix) {
+                t.Error("generateSentence was incorrect. Should have started with '" + sentencePrefix + "' and end with '" + sentenceSuffix + "' but was '" + filledSentence + "'")
+        }
 
 }
 
 //Generate sentences by passing in many patterns
 func TestFillManySentences(t *testing.T) {
-	var sentencePatterns = []struct {
-		Name    string
-		pattern string
-		prefix  string
-		suffix  string
-	}{
-		{"Original sentence template", "Aw yis, {{ adjective }} {{ nouns }}.", "Aw yis, ", "."},
-		{"Different prefix & suffix", "Hark! {{ adjective }} {{ nouns }}!", "Hark! ", "!"},
-		{"No prefix", "{{ adjective }} {{ nouns }} are at the door?", "", " are at the door?"},
-		{"Template with article", "Oh look, {{ an adjective }} {{ noun }}!", "Oh look, ", "!"},
-		{"Singular and plural", "Once I had {{ an adjective }} {{ noun }} full of {{ nouns }} but they flew into {{ a noun }}.", "Once I had ", "."},
-	}
+        var sentencePatterns = []struct {
+                Name    string
+                pattern string
+                prefix  string
+                suffix  string
+        }{
+                {"Original sentence template", "Aw yis, {{ adjective }} {{ nouns }}.", "Aw yis, ", "."},
+                {"Different prefix & suffix", "Hark! {{ adjective }} {{ nouns }}!", "Hark! ", "!"},
+                {"No prefix", "{{ adjective }} {{ nouns }} are at the door?", "", " are at the door?"},
+                {"Template with article", "Oh look, {{ an adjective }} {{ noun }}!", "Oh look, ", "!"},
+                {"Singular and plural", "Once I had {{ an adjective }} {{ noun }} full of {{ nouns }} but they flew into {{ a noun }}.", "Once I had ", "."},
+        }
 
-	for _, thisSentence := range sentencePatterns {
-		t.Run(thisSentence.Name, func(t *testing.T) {
-			filledSentence := fillSentence(thisSentence.pattern)
-			t.Log("Resulting sentence: " + filledSentence)
+        for _, thisSentence := range sentencePatterns {
+                t.Run(thisSentence.Name, func(t *testing.T) {
+                        filledSentence := fillSentence(thisSentence.pattern)
+                        t.Log("Resulting sentence: " + filledSentence)
 
-			if !strings.HasPrefix(filledSentence, thisSentence.prefix) && !strings.HasSuffix(filledSentence, thisSentence.suffix) {
-				t.Error("generateSentence was incorrect. Should have started with '" + thisSentence.prefix + "' and end with '" + thisSentence.suffix + "' but was '" + filledSentence + "'")
-			}
-		})
-	}
+                        if !strings.HasPrefix(filledSentence, thisSentence.prefix) && !strings.HasSuffix(filledSentence, thisSentence.suffix) {
+                                t.Error("generateSentence was incorrect. Should have started with '" + thisSentence.prefix + "' and end with '" + thisSentence.suffix + "' but was '" + filledSentence + "'")
+                        }
+                })
+        }
 
 }
 
 // Generate sentences by composing sententia functions
 // func TestFillSententiaSentencesWithActions(t *testing.T) {
-// 	var sentencePatterns = []struct {
-// 		pattern string
-// 		prefix  string
-// 		suffix  string
-// 	}{
-// 		{"She wrote a book called '{{ capitalize (an adjective) }} {{ capitalize noun }}'", "She wrote a book called '", ""},
-// 	}
+//      var sentencePatterns = []struct {
+//              pattern string
+//              prefix  string
+//              suffix  string
+//      }{
+//              {"She wrote a book called '{{ capitalize (an adjective) }} {{ capitalize noun }}'", "She wrote a book called '", ""},
+//      }
 
-// 	for _, thisSentence := range sentencePatterns {
-// 		filledSentence := fillSentence(thisSentence.pattern)
-// 		t.Log("Resulting sentence: " + filledSentence)
+//      for _, thisSentence := range sentencePatterns {
+//              filledSentence := fillSentence(thisSentence.pattern)
+//              t.Log("Resulting sentence: " + filledSentence)
 
-// 		if !strings.HasPrefix(filledSentence, thisSentence.prefix) && !strings.HasSuffix(filledSentence, thisSentence.suffix) {
-// 			t.Error("generateSentence was incorrect. Should have started with '" + thisSentence.prefix + "' and end with '" + thisSentence.suffix + "' but was '" + filledSentence + "'")
-// 		}
-// 	}
+//              if !strings.HasPrefix(filledSentence, thisSentence.prefix) && !strings.HasSuffix(filledSentence, thisSentence.suffix) {
+//                      t.Error("generateSentence was incorrect. Should have started with '" + thisSentence.prefix + "' and end with '" + thisSentence.suffix + "' but was '" + filledSentence + "'")
+//              }
+//      }
 
 // }
--- test/wordblanks_test.go
+++ test/wordblanks_test.go
@@ -1,54 +1,55 @@
 // +build unit
+
 package main_test
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+        "fmt"
+        "io"
+        "io/ioutil"
+        "net/http"
+        "net/http/httptest"
+        "testing"
 )
 
 //HTTP request to wordblanks
 /*
-	Nouns        []string `json:"noun"`
-	PresentVerbs []string `json:"verb_present"`
-	Verbs        []string `json:"verb"`
-	Adjectives   []string `json:"adjective"`
+        Nouns        []string `json:"noun"`
+        PresentVerbs []string `json:"verb_present"`
+        Verbs        []string `json:"verb"`
+        Adjectives   []string `json:"adjective"`
 
-	fmt.Println("Nouns list = ", record.Nouns)
-	fmt.Println("Present tense verbs = ", record.PresentVerbs)
-	fmt.Println("Verbs = ", record.Verbs)
-	fmt.Println("Adjectives = ", record.Adjectives)
+        fmt.Println("Nouns list = ", record.Nouns)
+        fmt.Println("Present tense verbs = ", record.PresentVerbs)
+        fmt.Println("Verbs = ", record.Verbs)
+        fmt.Println("Adjectives = ", record.Adjectives)
 */
 
 //Modeled after https://tutorialedge.net/golang/advanced-go-testing-tutorial/#mocking-http-requests
 func TestHttpGetRequestForWordBlanks(t *testing.T) {
-	token := "2UdLWTsozqnPNltOB4n2h6X8nf7lAH3m"
+        token := "2UdLWTsozqnPNltOB4n2h6X8nf7lAH3m"
 
-	sampleNouns := "[\"mission\",\"anchor\",\"yogurt\",\"amusement park\"]"
-	samplePresentVerbs := "[\"inspiring\",\"wobbling\",\"mailing\"]"
-	sampleVerbs := "[\"crack\",\"breathe\",\"climb\",\"sip\",\"make\"]"
-	sampleAdjectives := "[\"vociferous\",\"chatty\",\"rigorous\",\"scraggly\",\"strategic\",\"hurried\"]"
-	sampleResponse := "{\"noun\":[" + sampleNouns + ", \"verb_present\":[" + samplePresentVerbs + ", \"verb\":" + sampleVerbs + ", \"adjective\":" + sampleAdjectives + "}"
+        sampleNouns := "[\"mission\",\"anchor\",\"yogurt\",\"amusement park\"]"
+        samplePresentVerbs := "[\"inspiring\",\"wobbling\",\"mailing\"]"
+        sampleVerbs := "[\"crack\",\"breathe\",\"climb\",\"sip\",\"make\"]"
+        sampleAdjectives := "[\"vociferous\",\"chatty\",\"rigorous\",\"scraggly\",\"strategic\",\"hurried\"]"
+        sampleResponse := "{\"noun\":[" + sampleNouns + ", \"verb_present\":[" + samplePresentVerbs + ", \"verb\":" + sampleVerbs + ", \"adjective\":" + sampleAdjectives + "}"
 
-	targetUrl := "https://www.wordblanks.com/scripts/wb_ajax.php?token=" + token + "&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
+        targetUrl := "https://www.wordblanks.com/scripts/wb_ajax.php?token=" + token + "&method=getRandomWordSet&posArray%5B%5D=noun&posArray%5B%5D=verb_present&posArray%5B%5D=verb&posArray%5B%5D=adjective"
 
-	handler := func(writer http.ResponseWriter, request *http.Request) {
-		io.WriteString(writer, sampleResponse)
-	}
+        handler := func(writer http.ResponseWriter, request *http.Request) {
+                io.WriteString(writer, sampleResponse)
+        }
 
-	//Mock response
-	request := httptest.NewRequest("GET", targetUrl, nil)
-	writer := httptest.NewRecorder()
-	handler(writer, request)
+        //Mock response
+        request := httptest.NewRequest("GET", targetUrl, nil)
+        writer := httptest.NewRecorder()
+        handler(writer, request)
 
-	response := writer.Result()
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(body))
-	if 200 != response.StatusCode {
-		t.Fatal("Status code is " + string(response.StatusCode) + " not 200 OK.")
-	}
+        response := writer.Result()
+        body, _ := ioutil.ReadAll(response.Body)
+        fmt.Println(string(body))
+        if 200 != response.StatusCode {
+                t.Fatal("Status code is " + string(response.StatusCode) + " not 200 OK.")
+        }
 
 }
